4a3ee371e4c7fbbd6cc55873c72ec139eaf8f1e5
Bozhan Boyadzhiev <bozhanb@flutterint.com>
2026-08-05 16:52:34 +0300

    revert

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    const base = import.meta.env.BASE_URL || '/'
-    navigator.serviceWorker.register(`${base}sw.js`)
-  })
-}
